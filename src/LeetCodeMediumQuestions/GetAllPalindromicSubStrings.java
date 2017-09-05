e104d8d2ca5af51b168223dc7043eef9bb541041
susravan <sudhasravan92@gmail.com>
2017-09-04 22:43:38 -0700

    Return the number of palindromic strings that can be formed from the given input string

---
+++ src/LeetCodeMediumQuestions/GetAllPalindromicSubStrings.java
@@ -0,0 +1,47 @@
+package LeetCodeMediumQuestions;
+
+/**
+ * @author sravan created on Sep 4, 2017
+ *
+ */
+
+/*
+ * Given a string, your task is to count how many palindromic substrings in this
+ * string.
+ * 
+ * The substrings with different start indexes or end indexes are counted as
+ * different substrings even they consist of same characters.
+ * 
+ * Example 1: Input: "abc" Output: 3 Explanation: Three palindromic strings:
+ * "a", "b", "c". Example 2: Input: "aaa" Output: 6 Explanation: Six palindromic
+ * strings: "a", "a", "a", "aa", "aa", "aaa". Note: The input string length
+ * won't exceed 1000.
+ *
+ */
+
+
+public class GetAllPalindromicSubStrings {
+
+	public int getAllPalindromicSubStrings(String str) {
+		// Approach: Similar to LongestPalindromicSubstring problem
+		// For each of the 2*n - 1 centers, expand the string around the corner
+		int palinCount = 0;
+		
+		for(int i=0; i < str.length(); i++) {
+			palinCount += expandAroundCenter(str, i, i);
+			palinCount += expandAroundCenter(str, i, i + 1);
+		}
+		return palinCount;
+	}
+	
+	private int expandAroundCenter(String str, int L, int R) {
+		int count = 0;
+		while(L >= 0 && R < str.length() && str.charAt(L) == str.charAt(R)) {
+			count++;
+			L--;
+			R++;
+		}
+		return count;
+	}
+
+}
