31fdc1c5a84c83061f5ee7e469e738949fe79013
susravan <sudhasravan92@gmail.com>
2017-09-25 02:44:45 -0700

    Given an array of integers, return all possible permutations of it

--- src/LeetCodeMediumQuestions/Permutations.java
+++ src/LeetCodeMediumQuestions/Permutations.java
@@ -1,33 +1,50 @@
 package LeetCodeMediumQuestions;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
+
+/**
+ * @author sravan created on Sep 25, 2017
+ *
+ */
+
+/*
+ * Given a collection of distinct numbers, return all possible permutations.
+ * 
+ * For example, [1,2,3] have the following permutations: [ [1,2,3], [1,3,2],
+ * [2,1,3], [2,3,1], [3,1,2], [3,2,1] ]
+ *
+ */
 
 public class Permutations {
 
-	public List<List<Integer>> Permutations(int[] nums) {
+	public List<List<Integer>> getPermutations(int[] nums) {
 		List<List<Integer>> res = new ArrayList<>();
-        List<Integer> numsList = Arrays.asList(nums);
-        List<Integer> tempList = new ArrayList<>();
-        
-        permuteHelper(numsList, tempList, res);
-        return res;
+		List<Integer> numsList = new ArrayList<>();
+		for (int num : nums)
+			numsList.add(num);
+
+		List<Integer> tempList = new ArrayList<>();
+
+		res = permuteHelper(numsList, tempList, res);
+		return res;
 	}
 
-	private void permuteHelper(List<Integer> numList, List<Integer> perm, List<List<Integer>> res) {
-        if(numList.size() == 0) {
-            res.add(perm);
-            return;
-        }
-        
-        for(int i=0; i < numList.size(); i++) {
-            int currNum = numList.remove(i);
-            perm.add(currNum);
-            permuteHelper(numList, perm, res);
-            perm.remove(currNum);
-            numList.add(i, currNum);
-        }
-        return;
-    }
+	private List<List<Integer>> permuteHelper(List<Integer> numList, List<Integer> perm, List<List<Integer>> res) {
+		if (numList.size() == 0) {
+			res.add(new ArrayList<>(perm)); // ing a separate copy is required as an arraylist is an object and its
+											// address is passed when adding the list
+			return res; // This return res goes to previous permuteHelper but not to getPermutations
+						// method
+		}
+
+		for (int i = 0; i < numList.size(); i++) {
+			int currNum = numList.remove(i);
+			perm.add(currNum);
+			res = permuteHelper(numList, perm, res);
+			perm.remove(perm.size() - 1);
+			numList.add(i, currNum);
+		}
+		return res;
+	}
 }
