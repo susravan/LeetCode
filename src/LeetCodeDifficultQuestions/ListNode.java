ea5b6d5ef481e7ecd0469228f512d312e1c9e4c9
susravan <sudhasravan92@gmail.com>
2017-09-25 23:05:02 -0700

    Utility classes

---
+++ src/LeetCodeDifficultQuestions/ListNode.java
@@ -0,0 +1,21 @@
+package LeetCodeDifficultQuestions;
+
+/**
+ * @author sravan
+ * created on Aug 6, 2017
+ *
+ */
+
+/*  ListNode class common for the whole package
+ *
+ */
+
+
+public class ListNode {
+	 int val;
+	 ListNode next;
+	 
+	 ListNode(int x) { 
+		 val = x;
+		 }
+	 }
