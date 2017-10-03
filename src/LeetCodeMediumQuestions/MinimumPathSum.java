0007715a4b4ecff3e26d2034dc1a248e2ea2a381
susravan <sudhasravan92@gmail.com>
2017-10-02 23:34:07 -0700

    Given a matrix, find the path from top left to bottom right that minimizes the sum of all numbers along the path

---
+++ src/LeetCodeMediumQuestions/MinimumPathSum.java
@@ -0,0 +1,39 @@
+package LeetCodeMediumQuestions;
+
+/**
+ * @author sravan created on Oct 2, 2017
+ *
+ */
+
+/*
+ * Given a m x n grid filled with non-negative numbers, find a path from top
+ * left to bottom right which minimizes the sum of all numbers along its path.
+ * 
+ * Note: You can only move either down or right at any point in time.
+ *
+ */
+
+// Inspired from https://leetcode.com/problems/minimum-path-sum/discuss/
+public class MinimumPathSum {
+
+	public int getMinimumPathSum(int[][] grid) {
+		int m = grid.length;// row
+		int n = grid[0].length; // column
+		for (int i = 0; i < m; i++) {
+			for (int j = 0; j < n; j++) {
+				if (i == 0 && j != 0) {
+					grid[i][j] = grid[i][j] + grid[i][j - 1];
+				} else if (i != 0 && j == 0) {
+					grid[i][j] = grid[i][j] + grid[i - 1][j];
+				} else if (i == 0 && j == 0) {
+					grid[i][j] = grid[i][j];
+				} else {
+					grid[i][j] = Math.min(grid[i][j - 1], grid[i - 1][j]) + grid[i][j];
+				}
+			}
+		}
+
+		return grid[m - 1][n - 1];
+	}
+
+}
