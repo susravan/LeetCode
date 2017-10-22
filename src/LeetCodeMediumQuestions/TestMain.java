48cd096b0949449de4ee79d31d885414c82b1425
susravan <sudhasravan92@gmail.com>
2017-10-21 22:13:11 -0700

    Adding the main method

--- src/LeetCodeMediumQuestions/TestMain.java
+++ src/LeetCodeMediumQuestions/TestMain.java
@@ -1,27 +1,47 @@
 package LeetCodeMediumQuestions;
-import LeetCodeEasyQuestions.ValidPalindrome2;
-import linkedLists.*;
 
 public class TestMain {
 
 	public static void main(String args[]) {
-//		ListNode head = new ListNode(1);
-//		head.next = new ListNode(2);
-//		head.next.next = new ListNode(3);
-//		
-//		DeleteNthNodeFromLast dnn = new DeleteNthNodeFromLast();
+		// ListNode head = new ListNode(1);
+		// head.next = new ListNode(2);
+		// head.next.next = new ListNode(3);
+		//
+		// DeleteNthNodeFromLast dnn = new DeleteNthNodeFromLast();
+
+		// MapSumPairs msp = new MapSumPairs();
+		// msp.insert("Sravan", 5);
+		// msp.insert("Sra", 15);
+		// msp.insert("Sravan", 50);
+		// System.out.println(msp.sum("S"));
+
+		// ValidParanthesisString vps = new ValidParanthesisString();
+		// System.out.println(vps.checkValidParanthesisString("**)("));
+
+
+//		Permutations perm = new Permutations();
+//		System.out.println(perm.getPermutations(new int[] { 1, 2, 3}));
+
+//		PermutationsWithDuplicates permWithDup = new PermutationsWithDuplicates();
+//		System.out.println(permWithDup.getPermutationsWithDuplicates(new int[] {2,2,1}));
+
+//		Subsets sub = new Subsets();
+//		System.out.println(sub.getSubsets(new int[] {3,2,1}));
+		// NextClosestTime nc = new NextClosestTime();
+		// System.out.println(nc.getNextClosestTime("03:45"));
 		
-//		MapSumPairs msp = new MapSumPairs();
-//		msp.insert("Sravan", 5);
-//		msp.insert("Sra", 15);
-//		msp.insert("Sravan", 50);
-//		System.out.println(msp.sum("S"));
+//		RepeatedStringMatch rsm = new RepeatedStringMatch();
+//		System.out.println(rsm.getRepeatedStringMatch("sravan", "ravan"));
 		
-//		ValidParanthesisString vps = new ValidParanthesisString();
-//		System.out.println(vps.checkValidParanthesisString("**)("));
+//		PermutationSequence permSeq = new PermutationSequence();
+//		System.out.println(permSeq.getPermutationSequence(5,59));
 		
-		ValidPalindrome2 vp = new ValidPalindrome2();
-		System.out.println(vp.getValidPalindrome2("abcdcba"));
+//		DecodeWays dec = new DecodeWays();
+//		System.out.println(dec.numDecodings("303"));
+		
+//		NextClosestTime time = new NextClosestTime();
+//		System.out.println(time.getNextClosestTime("12:10"));
+		
 	}
 
 }
