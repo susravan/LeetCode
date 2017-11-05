bf0a01dbb1331b6928d997d702b5f0b31ffc5fbd
susravan <sudhasravan92@gmail.com>
2017-11-04 17:10:39 -0700

    Added comments to the logic

--- src/LeetCodeMediumQuestions/SpiralMatrix.java
+++ src/LeetCodeMediumQuestions/SpiralMatrix.java
@@ -22,25 +22,35 @@ import java.util.List;
 // Inspired from https://leetcode.com/problems/spiral-matrix/solution/
 public class SpiralMatrix {
 
-	public List<Integer> SpiralMatrix(int[][] matrix) {
-		List ans = new ArrayList();
-        if (matrix.length == 0)
-            return ans;
-        int r1 = 0, r2 = matrix.length - 1;
-        int c1 = 0, c2 = matrix[0].length - 1;
-        while (r1 <= r2 && c1 <= c2) {
-            for (int c = c1; c <= c2; c++) ans.add(matrix[r1][c]);
-            for (int r = r1 + 1; r <= r2; r++) ans.add(matrix[r][c2]);
-            if (r1 < r2 && c1 < c2) {
-                for (int c = c2 - 1; c > c1; c--) ans.add(matrix[r2][c]);
-                for (int r = r2; r > r1; r--) ans.add(matrix[r][c1]);
-            }
-            r1++;
-            r2--;
-            c1++;
-            c2--;
-        }
-        return ans;
+	public List<Integer> getSpiralMatrix(int[][] matrix) {
+		List<Integer> ans = new ArrayList<Integer>();
+		if (matrix.length == 0)
+			return ans;
+		int r1 = 0, r2 = matrix.length - 1;
+		int c1 = 0, c2 = matrix[0].length - 1;
+		while (r1 <= r2 && c1 <= c2) {
+			for (int c = c1; c <= c2; c++)
+				ans.add(matrix[r1][c]);
+			r1++;
+			for (int r = r1; r <= r2; r++)
+				ans.add(matrix[r][c2]);
+			c2--;
+			
+			// This condition is put to avoid writing separate result for base cases like
+			// single row and single column
+			// Otherwise this logic works fine for square and rectangular matrices
+			// Given a single row/ column would result in duplicate items if this check is
+			// not made
+			if (r1 <= r2 && c1 <= c2) {
+				for (int c = c2; c >= c1; c--)
+					ans.add(matrix[r2][c]);
+				r2--;
+				for (int r = r2; r >= r1; r--)
+					ans.add(matrix[r][c1]);
+				c1++;
+			}
+		}
+		return ans;
 	}
 
 }
