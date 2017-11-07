da0ad7fc8afc8ed3e9aff3a57e4d361f43904431
susravan <sudhasravan92@gmail.com>
2017-11-06 23:32:37 -0700

    Given a sorted matrix with element increasing from left to right and each row's first element is greater than the last row's last element, find the given element

--- src/LeetCodeMediumQuestions/Search2DMatrix.java
+++ src/LeetCodeMediumQuestions/Search2DMatrix.java
@@ -22,22 +22,29 @@ Given target = 5, return true.
 Given target = 20, return false.
  */
 
+// Inspired from https://leetcode.com/problems/search-a-2d-matrix/discuss/
 public class Search2DMatrix {
 	public boolean searchMatrix(int[][] matrix, int target) {
-        if(matrix == null || matrix.length < 1 || matrix[0].length <1) {
-            return false;
-        }
-        int col = matrix[0].length-1;
-        int row = 0;
-        while(col >= 0 && row <= matrix.length-1) {
-            if(target == matrix[row][col]) {
-                return true;
-            } else if(target < matrix[row][col]) {
-                col--;
-            } else if(target > matrix[row][col]) {
-                row++;
-            }
-        }
-        return false;
+		int row_num = matrix.length;
+		int col_num = matrix[0].length;
+		
+		int begin = 0, end = row_num * col_num - 1;
+		
+		while(begin <= end){
+			int mid = (begin + end) / 2;
+			int mid_value = matrix[mid/col_num][mid%col_num];
+			
+			if( mid_value == target){
+				return true;
+			
+			}else if(mid_value < target){
+				//Should move a bit further, otherwise dead loop.
+				begin = mid+1;
+			}else{
+				end = mid-1;
+			}
+		}
+		
+		return false;
     }
 }
