6fa4b6e6d06b7db102f0c2a6acc3a40ff5bdcb88
susravan <sudhasravan92@gmail.com>
2017-11-07 03:52:34 -0700

    Added dual binary search implementation

--- src/LeetCodeMediumQuestions/Search2DMatrix.java
+++ src/LeetCodeMediumQuestions/Search2DMatrix.java
@@ -22,29 +22,64 @@ Given target = 5, return true.
 Given target = 20, return false.
  */
 
-// Inspired from https://leetcode.com/problems/search-a-2d-matrix/discuss/
 public class Search2DMatrix {
 	public boolean searchMatrix(int[][] matrix, int target) {
-		int row_num = matrix.length;
-		int col_num = matrix[0].length;
 		
-		int begin = 0, end = row_num * col_num - 1;
-		
-		while(begin <= end){
-			int mid = (begin + end) / 2;
-			int mid_value = matrix[mid/col_num][mid%col_num];
-			
-			if( mid_value == target){
-				return true;
-			
-			}else if(mid_value < target){
-				//Should move a bit further, otherwise dead loop.
-				begin = mid+1;
-			}else{
-				end = mid-1;
-			}
+		// Approach - Dual binary search - first for getting the row, second for getting
+		// the element in the row
+		if (matrix.length == 0 || matrix[0].length == 0) {
+			return false;
 		}
-		
+
+		int len = matrix.length, wid = matrix[0].length;
+		int lo = 0, high = len - 1;
+
+		while (lo < high) {
+			int mid = lo + (high - lo + 1) / 2;
+			if (matrix[mid][0] <= target)
+				lo = mid;
+			else
+				high = mid - 1;
+		}
+		int fixRow = lo;
+
+		lo = 0;
+		high = wid - 1;
+		System.out.println("Fixed on row = " + fixRow);
+
+		while (lo <= high) {
+			int mid = lo + (high - lo) / 2;
+			if (matrix[fixRow][mid] == target)
+				return true;
+			else if (matrix[fixRow][mid] < target)
+				lo = mid + 1;
+			else
+				high = mid - 1;
+		}
 		return false;
+		
+//		 Approach - Treating the matrix as a single array of increasing sequence
+		
+		// int row_num = matrix.length;
+		// int col_num = matrix[0].length;
+		//
+		// int begin = 0, end = row_num * col_num - 1;
+		//
+		// while(begin <= end){
+		// int mid = (begin + end) / 2;
+		// int mid_value = matrix[mid/col_num][mid%col_num];
+		//
+		// if( mid_value == target){
+		// return true;
+		//
+		// }else if(mid_value < target){
+		// //Should move a bit further, otherwise dead loop.
+		// begin = mid+1;
+		// }else{
+		// end = mid-1;
+		// }
+		// }
+		//
+		// return false;
     }
 }
