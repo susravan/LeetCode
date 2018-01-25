5a5f9726c872826fb18d2d2131832d9b80fa23db
susravan <sudhasravan92@gmail.com>
2018-01-25 11:02:21 -0700

    Given 2 ints n and k, return all combinations of k numbers using 1..n

---
+++ src/LeetCodeMediumQuestions/Combinations.java
@@ -0,0 +1,42 @@
+package LeetCodeMediumQuestions;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author Sravan
+ * Created on Jan 25, 2018
+ */
+
+/**
+ * Given two integers n and k, return all possible combinations of k numbers out
+ * of 1 ... n.
+ * 
+ * For example, If n = 4 and k = 2, a solution is:
+ * 
+ * [ [2,4], [3,4], [2,3], [1,2], [1,3], [1,4], ]
+ */
+
+public class Combinations {
+	public List<List<Integer>> combine(int n, int k) {
+		List<List<Integer>> res = new ArrayList<>();
+		if (n < k || n == 0 || k == 0)
+			return res;
+
+		sumHelper(res, new ArrayList<Integer>(), n, 0, k);
+		return res;
+	}
+
+	// "start" is required to skip already used elements
+	private void sumHelper(List<List<Integer>> res, List<Integer> tempList, int n, int start, int k) {
+		if (tempList.size() == k)
+			res.add(new ArrayList<>(tempList));
+		else {
+			for (int i = start; i < n; i++) {
+				tempList.add(i + 1);
+				sumHelper(res, tempList, n, i + 1, k);
+				tempList.remove(tempList.size() - 1);
+			}
+		}
+	}
+}
