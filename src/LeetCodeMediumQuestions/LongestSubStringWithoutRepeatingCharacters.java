836b7a43ea55804cab539c4d27f806a2305941fa
susravan <sudhasravan92@gmail.com>
2018-02-14 01:49:27 -0700

    Updated the solution with single pass hashmap solution

--- src/LeetCodeMediumQuestions/LongestSubStringWithoutRepeatingCharacters.java
+++ src/LeetCodeMediumQuestions/LongestSubStringWithoutRepeatingCharacters.java
@@ -1,50 +1,51 @@
 package LeetCodeMediumQuestions;
 
 import java.util.HashMap;
-import java.util.HashSet;
 
 /**
- * @author sravan
- * created on Aug 23, 2017
+ * @author sravan created on Aug 23, 2017
  *
  */
 
-/*  Given a string, find the length of the longest substring without repeating characters.
-
-Examples:
-
-Given "abcabcbb", the answer is "abc", which the length is 3.
-
-Given "bbbbb", the answer is "b", with the length of 1.
-
-Given "pwwkew", the answer is "wke", with the length of 3. Note that the answer must be a substring, "pwke" is a subsequence and not a substring.
+/*
+ * Given a string, find the length of the longest substring without repeating
+ * characters.
+ * 
+ * Examples:
+ * 
+ * Given "abcabcbb", the answer is "abc", which the length is 3.
+ * 
+ * Given "bbbbb", the answer is "b", with the length of 1.
+ * 
+ * Given "pwwkew", the answer is "wke", with the length of 3. Note that the
+ * answer must be a substring, "pwke" is a subsequence and not a substring.
  *
  */
 
-// Inspired from https://leetcode.com/problems/longest-substring-without-repeating-characters/solution/
+// Inspired from
+// https://leetcode.com/problems/longest-substring-without-repeating-characters/solution/
 
 public class LongestSubStringWithoutRepeatingCharacters {
 
 	public int getLongestSubStringWithoutRepeatingCharacters(String s) {
-		if(s.length() < 2)
-			return s.length();
-		
+		// left - start of substring, right - pointer to parse the string
+		// Use a hashmap to put a character and it's index. If a character is already
+		// seen, update left pointer to next to seen character, update its index in map.
+		// Update maxLen at each step
+		// There is no need to move back the left pointer at any point in time
 		int left = 0, right = 0;
 		HashMap<Character, Integer> map = new HashMap<>();
 		int maxLen = 0;
-		
-		while(right < s.length()) {
+
+		for (right = 0; right < s.length(); right++) {
 			char currChar = s.charAt(right);
-			if(!map.containsKey(currChar))
-				map.put(currChar, right);
-			else {
-				left = map.get(currChar);
-				maxLen = Math.max(maxLen, right - left);
-				System.out.println("Right = " + right + " left = " + left);
-				
-				
+			if (map.containsKey(currChar)) {
+				left = Math.max(map.get(currChar) + 1, left);
 			}
-			right++;
+			map.put(currChar, right);
+			maxLen = Math.max(maxLen, right - left + 1);
+			// System.out.println("left = " + left + " right = " + right + " size = " +
+			// map.size());
 		}
 		return maxLen;
 	}
