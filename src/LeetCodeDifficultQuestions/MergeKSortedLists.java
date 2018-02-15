d1c9f76bf2f7dfa28fc278da17ccd38492627a40
susravan <sudhasravan92@gmail.com>
2018-02-14 20:23:54 -0700

    Added divide and conquer solution

--- src/LeetCodeDifficultQuestions/MergeKSortedLists.java
+++ src/LeetCodeDifficultQuestions/MergeKSortedLists.java
@@ -16,33 +16,68 @@ import java.util.PriorityQueue;
 // Inspired from https://leetcode.com/problems/merge-k-sorted-lists/solution/
 public class MergeKSortedLists {
 	public ListNode mergeKSortedLists(ListNode[] lists) {
-		// Using a priority Queue
-		ListNode res = new ListNode(0);
-		ListNode dummy = res;
-		PriorityQueue<ListNode> pq = new PriorityQueue<>(new Comparator<ListNode>() {
-			@Override
-			public int compare(ListNode o1, ListNode o2) {
-				return o1.val - o2.val;
-			}
-		});
+		// Divide and conquer approach with one list as base case and merging 2 sorted
+		// lists
+		if (lists.length == 0)
+			return null;
 
-		// Add all start nodes
-		for (int i = 0; i < lists.length; i++) {
-			if (lists[i] != null)
-				pq.add(lists[i]);
-		}
+		return mergeKListsHelper(lists, 0, lists.length - 1);
 
-		// Until pq is empty take one out from it and add to res
-		while (pq.size() != 0) {
-			ListNode nextNode = pq.poll();
-			res.next = nextNode;
-			res = res.next;
-			if (nextNode.next != null) {
-				nextNode = nextNode.next;
-				pq.add(nextNode);
-			}
-		}
-		return dummy.next;
+		// // Using a priority Queue
+		// ListNode res = new ListNode(0);
+		// ListNode dummy = res;
+		// PriorityQueue<ListNode> pq = new PriorityQueue<>(new Comparator<ListNode>() {
+		// @Override
+		// public int compare(ListNode o1, ListNode o2) {
+		// return o1.val - o2.val;
+		// }
+		// });
+		//
+		// // Add all start nodes
+		// for (int i = 0; i < lists.length; i++) {
+		// if (lists[i] != null)
+		// pq.add(lists[i]);
+		// }
+		//
+		// // Until pq is empty take one out from it and add to res
+		// while (pq.size() != 0) {
+		// ListNode nextNode = pq.poll();
+		// res.next = nextNode;
+		// res = res.next;
+		// if (nextNode.next != null) {
+		// nextNode = nextNode.next;
+		// pq.add(nextNode);
+		// }
+		// }
+		// return dummy.next;
 	}
 
+	private ListNode mergeKListsHelper(ListNode[] lists, int lo, int high) {
+		if (lo == high)
+			return lists[lo];
+
+		int mid = lo + (high - lo) / 2;
+		ListNode leftNode = mergeKListsHelper(lists, lo, mid);
+		ListNode rightNode = mergeKListsHelper(lists, mid + 1, high);
+
+		// Merge 2 lists
+		ListNode res = new ListNode(0);
+		ListNode dummy = res;
+
+		while (leftNode != null && rightNode != null) {
+			if (leftNode.val < rightNode.val) {
+				res.next = leftNode;
+				leftNode = leftNode.next;
+			} else {
+				res.next = rightNode;
+				rightNode = rightNode.next;
+			}
+			res = res.next;
+		}
+		if (leftNode != null)
+			res.next = leftNode;
+		if (rightNode != null)
+			res.next = rightNode;
+		return dummy.next;
+	}
 }
