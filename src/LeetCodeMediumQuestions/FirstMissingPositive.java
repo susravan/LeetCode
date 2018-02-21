86fb177a5a10a1f662fca11d0960e6c93565f94e
susravan <sudhasravan92@gmail.com>
2018-02-20 23:47:09 -0700

    Updated comments with better structured code

--- src/LeetCodeMediumQuestions/FirstMissingPositive.java
+++ src/LeetCodeMediumQuestions/FirstMissingPositive.java
@@ -1,9 +1,8 @@
 package LeetCodeMediumQuestions;
 
-import java.util.Arrays;
-
 /**
- * @author Sravan Given an unsorted integer array, find the first missing
+ * @author Sravan
+ * Given an unsorted integer array, find the first missing
  *         positive integer.
  * 
  *         For example, Given [1,2,0] return 3, and [3,4,-1,1] return 2.
@@ -13,49 +12,45 @@ import java.util.Arrays;
  * 
  */
 
-// Inspired from
-// https://leetcode.com/problems/first-missing-positive/discuss/17073
+// Inspired from https://leetcode.com/problems/first-missing-positive/discuss/17073
 public class FirstMissingPositive {
 	public int firstMissingPositive(int[] nums) {
-		// Approach: Restructure the array to get all positive numbers first and then
-		// negative numbers, use first k positive numbers to make values at those
-		// indicies negative. Finally the first positive number is out result
-		if (nums.length == 0)
-			return 1;
-
-		int left = 0, right = nums.length - 1;
-
-		while (left < right) {
-			if (nums[left] <= 0) {
-				swap(nums, left, right);
-				right--;
-			} else {
-				left++;
-			}
-		}
-		// Update positive numbers' index values
-		for (int i = 0; i <= left; i++) {
-			int index = Math.abs(nums[i]);
-			if (index - 1 <= left) {
-				nums[index - 1] = nums[index - 1] > 0 ? -nums[index - 1] : nums[index - 1];
-			}
-		}
-		System.out.println(Arrays.toString(nums));
-		System.out.println(left);
-
-		// Depending on index instead of actual values in the array => there cannot be
-		// duplicates that masks the direct correlation between indicies and its values
-		for (int i = 0; i <= left; i++) {
-			if (nums[i] > 0)
-				return i + 1;
-		}
-		return left + 2; // left is max postitive index, so add +2 to get number - one for index, one for
-							// next value
-	}
-
-	private void swap(int[] nums, int i1, int i2) {
-		int temp = nums[i1];
-		nums[i1] = nums[i2];
-		nums[i2] = temp;
-	}
+        // k has the first non-positive number';'s index in the seggregated array
+        int maxPositiveIndex = seggregateArray(nums);
+        // System.out.println("k = " + k + Arrays.toString(nums));
+        
+        for(int i=0; i < maxPositiveIndex; i++) {
+            int absValue = Math.abs(nums[i]);
+            if(absValue <= maxPositiveIndex) {
+                nums[absValue-1] = nums[absValue-1] > 0 ? -nums[absValue-1] : nums[absValue-1];
+            }
+        }
+        // System.out.println(Arrays.toString(nums));
+        int i=0;
+        for(i=0; i < maxPositiveIndex; i++) {
+            if(nums[i] > 0)
+                return i+1;
+        }
+        return i+1;
+    }
+    
+    private int seggregateArray(int[] arr) {
+        int lo=0, high = arr.length-1;
+        while(lo <= high) {
+            System.out.println(lo + ", " + high);
+            while(lo < arr.length && arr[lo] > 0)
+                lo++;
+            System.out.println(high + ", " + arr[high]);
+            while(high >= 0 && arr[high] <= 0)
+                high--;
+            if(lo < high) {
+                int temp = arr[lo];
+                arr[lo] = arr[high];
+                arr[high] = temp;
+                lo++;
+                high--;
+            }
+        }
+        return lo;
+    }
 }
