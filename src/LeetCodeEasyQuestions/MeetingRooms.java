186dc9162826af11e8f11d75ceb2b4f1d74c2b1d
susravan <sudhasravan92@gmail.com>
2018-02-26 18:05:28 -0700

    Made Interval a separate class

--- src/LeetCodeEasyQuestions/MeetingRooms.java
+++ src/LeetCodeEasyQuestions/MeetingRooms.java
@@ -32,19 +32,4 @@ public class MeetingRooms {
 		}
 		return true;
 	}
-
-	public class Interval {
-		int start;
-		int end;
-
-		Interval() {
-			start = 0;
-			end = 0;
-		}
-
-		Interval(int s, int e) {
-			start = s;
-			end = e;
-		}
-	}
 }
