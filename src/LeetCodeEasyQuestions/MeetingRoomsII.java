94d646bcb2041d35bf301193dedb1e4cfb26210c
susravan <sudhasravan92@gmail.com>
2018-02-26 18:46:02 -0700

    Find no. of rooms required given a meeting schedule

---
+++ src/LeetCodeEasyQuestions/MeetingRoomsII.java
@@ -0,0 +1,35 @@
+package LeetCodeEasyQuestions;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+/**
+ * @author Sravan
+ * Created on Feb 26, 2018
+ */
+
+/**
+ *  Given an array of meeting time intervals consisting of start and end times [[s1,e1],[s2,e2],...] (si < ei), determine if a person could attend all meetings.
+	For example,
+	Given [[0, 30],[5, 10],[15, 20]],
+	return false.
+ */
+
+public class MeetingRooms {
+	public boolean canAttendMeetings(Interval[] intervals) {
+		if (intervals.length == 0)
+			return true;
+		Arrays.sort(intervals, new Comparator<Interval>() {
+			@Override
+			public int compare(Interval i1, Interval i2) {
+				return i1.start - i2.start;
+			}
+		});
+
+		for (int i = 1; i < intervals.length; i++) {
+			if (intervals[i - 1].end > intervals[i].start)
+				return false;
+		}
+		return true;
+	}
+}
