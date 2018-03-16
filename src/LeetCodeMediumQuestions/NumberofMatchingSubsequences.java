5b4d158639afc7f89a53602684ce007e75b5845d
susravan <sudhasravan92@gmail.com>
2018-03-16 01:05:57 -0700

    Added description

--- src/LeetCodeMediumQuestions/NumberofMatchingSubsequences.java
+++ src/LeetCodeMediumQuestions/NumberofMatchingSubsequences.java
@@ -2,6 +2,29 @@ package LeetCodeMediumQuestions;
 
 import java.util.ArrayList;
 
+/**
+ * @author Sravan
+ * Created on Mar 16, 2018
+ */
+
+/**
+ * Given string S and a dictionary of words words, find the number of words[i] that is a subsequence of S.
+
+Example :
+Input: 
+S = "abcde"
+words = ["a", "bb", "acd", "ace"]
+Output: 3
+Explanation: There are three words in words that are a subsequence of S: "a", "acd", "ace".
+Note:
+
+All words in words and S will only consists of lowercase letters.
+The length of S will be in the range of [1, 50000].
+The length of words will be in the range of [1, 5000].
+The length of words[i] will be in the range of [1, 50].
+ */
+
+// Inspired from 
 public class NumberofMatchingSubsequences {
 	public int numMatchingSubseq(String str, String[] words) {
 		if(str.equals("") || words.length == 0)
