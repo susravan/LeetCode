402d7d1d37a99edf0daff7f9489a5d47ce5bbcc2
susravan <sudhasravan92@gmail.com>
2018-03-29 21:42:29 -0700

    Added optimized method to calculate optimal point in O(MN) space and time

--- src/LeetCodeDifficultQuestions/BestMeetingPoint.java
+++ src/LeetCodeDifficultQuestions/BestMeetingPoint.java
@@ -24,6 +24,49 @@ The point (0,2) is an ideal meeting point, as the total travel distance of 2+2+2
 
 // Inspired from https://leetcode.com/problems/best-meeting-point/solution/
 public class BestMeetingPoint {
+	// This method doesn't need sorting as we are taking the coordinates in sorted
+	// order - O(MN) both space and time
+	public int minTotalDistanceOptimized(int[][] grid) {
+
+		List<Integer> rows = getXCords(grid);
+		List<Integer> cols = getYCords(grid);
+		return minDist1D(rows) + minDist1D(cols);
+	}
+
+	// Minimum distance can be calculated without calculating the median
+	private int minDist1D(List<Integer> list) {
+		int lo = 0, high = list.size() - 1;
+		int dist = 0;
+		while (lo < high) {
+			dist += Math.abs(list.get(high) - list.get(lo));
+			lo++;
+			high--;
+		}
+		return dist;
+	}
+
+	private List<Integer> getXCords(int[][] grid) {
+		List<Integer> rows = new ArrayList<>();
+		for (int row = 0; row < grid.length; row++) {
+			for (int col = 0; col < grid[0].length; col++) {
+				if (grid[row][col] == 1)
+					rows.add(row);
+			}
+		}
+		return rows;
+	}
+
+	private List<Integer> getYCords(int[][] grid) {
+		List<Integer> cols = new ArrayList<>();
+		for (int col = 0; col < grid[0].length; col++) {
+			for (int row = 0; row < grid.length; row++) {
+				if (grid[row][col] == 1)
+					cols.add(col);
+			}
+		}
+		return cols;
+	}
+
 	// Think of how you would do if the problem is in 1D and extend that to 2D
 	// 1D method - Find the median of the house's locations and calculate the
 	// distances from each house to median - O(MNlog(MN)) time and O(MN) space
@@ -40,13 +83,13 @@ public class BestMeetingPoint {
 			}
 		}
 		int row = rows.get(rows.size() / 2);
-		Collections.sort(cols);	// Doesn't need sorting for rows
+		Collections.sort(cols); // Doesn't need sorting for rows
 		int col = cols.get(cols.size() / 2);
 
-		return get1Ddist(rows, row) + get1Ddist(cols, col);
+		return minDist1D(rows, row) + minDist1D(cols, col);
 	}
-	
-	private int get1Ddist(List<Integer> rows, int origin) {
+
+	private int minDist1D(List<Integer> rows, int origin) {
 		int dist = 0;
 		for (int point : rows)
 			dist += Math.abs(point - origin);
